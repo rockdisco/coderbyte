9d4a331a59c6b1b483a3a206aa4313fda78a956b
Ian Bahc <iwantus@hanmail.net>
2015-08-03 03:38:29 +0900

    DashInsertII.rb

--- DashInsertII.rb
+++ DashInsertII.rb
@@ -1,8 +1,10 @@
 def DashInsertII(num)
   arr=num.to_s.split('')
   arr.each_index do |i|
+    next if arr[i].to_i == 0
+    next if i == arr.length - 1
     arr[i].concat('-') if arr[i].to_i.odd? && arr[i + 1].to_i.odd?
-    arr[i].concat('*') if arr[i] != 0 && arr[i].to_i.even? && arr[i + 1].to_i.even?
+    arr[i].concat('*') if arr[i].to_i.even? && arr[i + 1].to_i.even?
   end.join
 end
 DashInsertII(STDIN.gets)
