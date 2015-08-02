c947840208e55b53257b163f491d727882b7b3e6
Ian Bahc <iwantus@hanmail.net>
2015-08-02 19:52:59 +0900

    Update and rename DashInsertII to DashInsertII.rb

--- DashInsertII.rb
+++ DashInsertII.rb
@@ -2,7 +2,7 @@ def DashInsertII(num)
   arr=num.to_s.split('')
   arr.each_index do |i|
     arr[i].concat('-') if arr[i].to_i.odd? && arr[i + 1].to_i.odd?
-    arr[i].concat('*') if arr[i].to_i.even? && arr[i + 1].to_i.even?
+    arr[i].concat('*') if arr[i] != 0 && arr[i].to_i.even? && arr[i + 1].to_i.even?
   end.join
 end
 DashInsertII(STDIN.gets)
