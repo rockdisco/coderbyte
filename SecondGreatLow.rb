78e6d4d6cd2485756d75fd21d1dbb4281dfbcbbb
Ian Bahc <iwantus@hanmail.net>
2015-08-02 18:42:44 +0900

    SecondGreatLow.rb

--- SecondGreatLow.rb
+++ SecondGreatLow.rb
@@ -1,5 +1,6 @@
 def SecondGreatLow(arr)
   arr = arr.sort.uniq
-  return "#{arr[1]} #{arr[-2]}"   
+  return "#{arr[0]} #{arr[0]}" if arr.length == 1
+  return "#{arr[1]} #{arr[-2]}"
 end
 SecondGreatLow(STDIN.gets)
