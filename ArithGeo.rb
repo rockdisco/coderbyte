fe6a7359e850d40276234ec07d5fdcb29ffc51b4
Ian Bahc <iwantus@hanmail.net>
2015-08-03 22:41:33 +0900

    ArithGeo.rb

--- ArithGeo.rb
+++ ArithGeo.rb
@@ -1,6 +1,11 @@
 def ArithGeo(arr)
   return -1 if arr.count(0) > 0
-  return "Arithmetic" if arr.each_cons(2).all? {|x,y| y - x == arr[2] - arr[1]}
-  return "Geometric" if arr.each_cons(2).all? {|x,y| y / x == arr[2] / arr[1]}
+  if arr.each_cons(2).all? {|x,y| y - x == arr[2] - arr[1]}
+    return "Arithmetic" 
+  elseif arr.each_cons(2).all? {|x,y| y / x == arr[2] / arr[1]}
+  	return "Geometric"
+  else
+    return -1
+  end
 end
-ArithGeo(STDIN.gets)
+ArithGeo(STDIN.gets) 
