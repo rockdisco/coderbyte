387d97c7f4de5b5a46eb6cd704e9edc451026ae1
Ian Bahc <iwantus@hanmail.net>
2015-08-04 00:58:04 +0900

    CountingMinutesI.rb

--- CountingMinutesI.rb
+++ CountingMinutesI.rb
@@ -1,14 +1,13 @@
 def CountingMinutesI(str)
-  t1, t2 = str.split('-')
-  
-  h1, m1= t1.split(':')[0].to_i, t1.split(':')[1].to_i
-  h1 = h1 + 12 if t1.include?('pm')
+  arr= str.split('-').to_a
 
-  h2, m2= t2.split(':')[0].to_i, t2.split(':')[1].to_i
-  h2 = h2 + 12 if t2.include?('pm')
+  h1, m1= arr[0].split(':')[0].to_i, arr[0].split(':')[1].to_i
+  h1 = h1 + 12 if arr[0].include?('pm')
+
+  h2, m2= arr[1].split(':')[0].to_i, arr[1].split(':')[1].to_i
+  h2 = h2 + 12 if arr[1].include?('pm')
 
   sum = ((h2-h1)*60)+(m2 - m1)
-  sum += 24*60 if sum < 0
-return sum
+  return sum > 0 ? sum : sum += 24*60
 end
 CountingMinutesI(STDIN.gets)
