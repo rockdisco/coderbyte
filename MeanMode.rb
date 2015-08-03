39d323d26ebcca81b8e7a46ca1fc56b2f468e349
Ian Bahc <iwantus@hanmail.net>
2015-08-04 00:15:43 +0900

    MeanMode.rb

--- MeanMode.rb
+++ MeanMode.rb
@@ -1,17 +1,17 @@
 def MeanMode(arr)
   mean = arr.inject(:+).fdiv(arr.size)
-
-  mv = 1
-  mode = 0
-  arr.each do |c|
-    cnt = arr.count(c)
-    if mv < cnt
-      mv = cnt
-      mode = c
-    end
-  end
-  
+  mode = arr.max_by {|w| arr.count(w) }
   return mean == mode ? 1 : 0
-  
 end
 MeanMode(STDIN.gets)
+
+#처음에 짜본 최빈값 알고리즘
+# mv = 1
+#  mode = 0
+#  arr.each do |c|
+#    cnt = arr.count(c)
+#    if mv < cnt
+#      mv = cnt
+#      mode = c
+#    end
+#  end
